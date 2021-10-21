ff411be290023d8746b73688860681ff1ad6285f
Virpinchk <91627169+Virpinchk@users.noreply.github.com>
2021-10-21 17:16:36 +0500

    Add files via upload

--- платформер/player.gd
+++ платформер/player.gd
@@ -1,9 +1,35 @@
 extends KinematicBody2D
- 
+
+const Floor = Vector2(0,-1)
+
+var OnAirTime = 0
+
+var accel=Vector2()
+
+var speed = 300
+
+const GRAVETE = Vector2(0,980)
+
 var vert_move=0
 
-func _physics_process(delta):
-	var accel=Vector2()
-	accel.x += 1 if Input.is_action_pressed("right") else 0
-	accel.x -= 1 if Input.is_action_pressed("left") else 0
-	vert_move=-300 if Input.is_action_pressed("fump") else vert_move + 9.6 * delta
+func _process(delta):
+	accel += GRAVETE * delta
+	var target_vel = 0
+	target_vel =- int(Input.is_action_pressed("left")) + int(Input.is_action_pressed("right"))
+	OnAirTime += delta
+	accel = move_and_slide(accel,Floor,25.0)
+	if is_on_floor():
+		OnAirTime = 0
+	var on_floor = OnAirTime < 0.1
+	target_vel *= speed
+	accel.x = lerp(accel.x,target_vel,0.3)
+	#accel.x += speed if Input.is_action_pressed("right") else 0
+	#accel.x -= speed if Input.is_action_pressed("left") else 0
+	#vert_move=-300 if Input.is_action_pressed("fump") else vert_move + 9.6 * delta
+	#accel += GRAVETE
+	#accel = move_and_slide(accel+Vector2(0,vert_move),Vector2)
+	
+	
+	
+	
+	
