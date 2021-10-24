1681478356bef80300659bd9694a1a44179f35c8
Virpinchk <91627169+Virpinchk@users.noreply.github.com>
2021-10-24 22:27:45 +0500

    Add files via upload

--- платформер/player.gd
+++ платформер/player.gd
@@ -22,11 +22,31 @@ func _process(delta):
 	target_vel =- int(Input.is_action_pressed("left")) + int(Input.is_action_pressed("right"))
 	OnAirTime += delta
 	accel = move_and_slide(accel,Floor,25.0)
+	
+	animate()
+	
 	if is_on_floor():
 		OnAirTime = 0
 	var on_floor = OnAirTime < 0.1
 	target_vel *= speed
 	accel.x = lerp(accel.x,target_vel,0.3)
+	
+func animate():
+	var anim = "idle"
+	
+	if accel.y > 0 and not $RayCast2D.is_colliding():
+		anim = "jump"
+	elif accel.y < 0 and not $RayCast2D.is_colliding():
+		anim = "fall"
+	else:
+		if accel.x != 0:
+			anim = "run"
+			if accel.x > 0:
+				$Sprite.flip_h = false
+			elif accel.x < 0:
+				$Sprite.flip_h = true
+	if $Sprite.animation != anim:
+		$Sprite.play(anim)
 	#accel.x += speed if Input.is_action_pressed("right") else 0
 	#accel.x -= speed if Input.is_action_pressed("left") else 0
 	#vert_move=-300 if Input.is_action_pressed("fump") else vert_move + 9.6 * delta
